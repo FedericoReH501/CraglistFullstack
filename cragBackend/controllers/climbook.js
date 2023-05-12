3d0192a667ec5b8fa25d0fecee45a77fc95b2c77
Federico Re <federico.re1996@gmail.com>
2023-05-12 17:23:57 +0300

    v4

--- cragBackend/controllers/climbook.js
+++ cragBackend/controllers/climbook.js
@@ -4,26 +4,36 @@ const axios = require('axios')
 const { request, response } = require('express')
 const allRegions = [3,32,19,5,41,81,1,15,20,9,4,37,111,21,16,6,7,2,29,33]
 let array=[]
+
 const fetcher =async (link)=>{
   for (let index = 0; index < allRegions.length; index++) {
     const element= allRegions[index]
     const result = await axios.get(`${link}${element}`)
     array.push(result.data)
   }
+  return array
+}
 
+const cragsFetcher =async (link)=>{
+  
+  const result = await axios.get(`${link}/vie`)
+  
+  return result.data
 }
 
 climbookRouter.get('/',async (request,response)=>{
   //const result = await axios.get(`https://climbook.com/falesie`)
-  await fetcher('https://climbook.com/regioni/')
+  const result = await fetcher('https://climbook.com/regioni/')
   
-  response.status(200).send(array)
+  response.status(200).send(result)
   array=[]
 })
 
 climbookRouter.put('/',async (request,response)=>{
-  console.log(request.body)
-  response.status(200).json({result:'daje'})
+  const link = request.body.link
+  
+  const result = await cragsFetcher(link)
+  response.status(200).send(result)
 
 })
 
