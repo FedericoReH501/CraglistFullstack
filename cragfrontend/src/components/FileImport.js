3a8bb99f8d4a03dde3ed6179db27ce52577b5217
Federico Re <federico.re1996@gmail.com>
2023-05-12 17:23:52 +0300

    v.4

--- cragfrontend/src/components/FileImport.js
+++ cragfrontend/src/components/FileImport.js
@@ -1,48 +1,8 @@
 import axios from 'axios'
 
-var xlsx = require("xlsx")
+
 const FileImport = ()=>{
     
-  
-  let html = ''
-  const iterator = (file)=>{
-    for (let index = 1; index < 18; index++) {
-      const name = file[`A${index}`].v
-      const grade = file[`B${index}`].v
-
-      const result = {
-        name,
-        grade
-      }
-      
-      //for(let i =1,)
-    }
-  }
-  function handleFile(e) {
-        e.preventDefault()
-         if (e.target.file.files) {
-          const reader = new FileReader()
-          
-          reader.onload = (e) => {
-            
-               const data = e.target.result
-               const workbook = xlsx.read(data, {type: '"array'})
-               const sheetName= workbook.SheetNames[0]
-               //console.log('name', sheetName)
-               //console.log(workbook.Sheets[sheetName]['A1'].v)
-              const file = workbook.Sheets[sheetName]
-              
-              iterator(file)
-               // organize xlsx data into desired format
-          
-          }
-          reader.readAsArrayBuffer(e.target.file.files[0])
-    
-         }
-         
-
-    }
-
   const download = async (e)=>{
     e.preventDefault()
     const result = await axios.get('climbook')
@@ -71,39 +31,52 @@ const FileImport = ()=>{
         const cragsRaw = tab.querySelectorAll("tbody tr a") // lista dei tr contenenti il nome della falesia in a
         for (let index = 0; index < cragsRaw.length; index++) { // per ogni crag
           const crag = cragsRaw[index];
+          const cragName= crag.textContent
           const link = crag.href
-          console.log('try')
-          const response = await axios.put('climbook',{link})
-          console.log('passed')
+          
+          const response = await axios.put('climbook',{link}) // ritorna html della falesia
+          const doc = parser.parseFromString(response.data, 'text/html');//html con le vie
+          //ogni crag ha una tabella con la lista delle vie
+          // ogni tabella ha il nome nel link a, grado nel span
+
+          const nomiVie = doc.querySelectorAll("table.cb-table tbody tr a.font-semibold")
+          const gradiVie = doc.querySelectorAll("table.cb-table tbody tr span.font-semibold")
+          let vieList =[]
+          for (let index = 0; index < nomiVie.length; index++) {
+            const name = nomiVie[index].textContent
+            const grade = gradiVie[index].textContent
+            vieList.push({
+              name,
+              grade
+            })
+            
+          }
+
+          console.log('Crag Name: ',cragName)
+          console.log('Regione: ',region)
+          console.log('Provincia: ',provincia)
+          console.log('Lista Vie:')
+          vieList.forEach(v=>console.log(v))
+          console.log('---------------------------------------------------------------')
+          console.log('---------------------------------------------------------------')
+          
+        
         }
-        console.log('cragsraw:')
-        cragsRaw.forEach(c=>console.log('Crag Name:',c.textContent,', Regione:',region, ', Provincia:',provincia,', Link:',c.href))
+        //console.log('cragsraw:')
+        //cragsRaw.forEach(c=>console.log('Crag Name:',c.textContent,', Regione:',region, ', Provincia:',provincia,', Link:',c.href))
         // posso avere per ogni crag la sua regione e provincia
-        console.log('-------')
+       // console.log('-------')
       }
 
       
-      
-      const provinceRaw = doc.querySelectorAll("th.px-0")
-      provinceRaw.forEach(r=>{
-        if(r.className ==='px-0 py-0 md:py-1 text-xl font-bold'){
-          province.push(r.textContent)}
-        })
+
       
     }
 
-    const doc = parser.parseFromString(result.data, 'text/html');
     
-    const regions = doc.querySelectorAll("title")
     
 
-    for (let index = 0; index < regions.length; index++) {
-
-      const elem = regions[index]
-      const string = elem.textContent
-      const length = string.length
-       
-    }
+    
   }
 
   return(
