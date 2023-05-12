bbe84719a0840c21e0db4a7979e1475831cff63f
Federico Re <federico.re1996@gmail.com>
2023-05-12 16:07:54 +0300

    v.3

--- cragBackend/controllers/climbook.js
+++ cragBackend/controllers/climbook.js
@@ -1,12 +1,13 @@
 const climbookRouter = require('express').Router()
 
 const axios = require('axios')
+const { request, response } = require('express')
 const allRegions = [3,32,19,5,41,81,1,15,20,9,4,37,111,21,16,6,7,2,29,33]
 let array=[]
-const fetcher =async ()=>{
+const fetcher =async (link)=>{
   for (let index = 0; index < allRegions.length; index++) {
     const element= allRegions[index]
-    const result = await axios.get(`https://climbook.com/regioni/${element}`)
+    const result = await axios.get(`${link}${element}`)
     array.push(result.data)
   }
 
@@ -14,11 +15,16 @@ const fetcher =async ()=>{
 
 climbookRouter.get('/',async (request,response)=>{
   //const result = await axios.get(`https://climbook.com/falesie`)
-  await fetcher()
+  await fetcher('https://climbook.com/regioni/')
   
   response.status(200).send(array)
   array=[]
 })
-  
+
+climbookRouter.put('/',async (request,response)=>{
+  console.log(request.body)
+  response.status(200).json({result:'daje'})
+
+})
 
 module.exports = climbookRouter
--- cragfrontend/src/components/FileImport.js
+++ cragfrontend/src/components/FileImport.js
@@ -48,15 +48,61 @@ const FileImport = ()=>{
     const result = await axios.get('climbook')
     
     let parser = new DOMParser();
-    const doc = parser.parseFromString(result.data, 'text/html');
+    
+    const arrayData = result.data
+    for (let index = 0; index < arrayData.length; index++) {//per ogni pagina di regione
+      const province =[]
+      const element = arrayData[index];
+      const doc = parser.parseFromString(element, 'text/html');
+      //doc contiene html della pagina, il nome regione e' nel titolo 
+      //ogni provincia e' contenuta in una tabella
+      const stringRegion = doc.querySelectorAll("title")[0].textContent
+      const regionLength = stringRegion.length
+      const region = stringRegion.slice(8,regionLength-11)
+      // salvo il nome della regione dal titolo della pagina
+      // ogni tabella contiene nel thead il nome della provincia nel tbody la lista delle crags
+      const tablesRaw = doc.querySelectorAll("table.w-full") // trovo tutte le tabelle
 
+      for (let index = 0; index < tablesRaw.length; index++) {// per ogni tabella(provincia)
+        const tab = tablesRaw[index]
+        const provincia = tab.querySelectorAll("thead th.px-0")[0].textContent
+        province.push(provincia)
+        // ogni tbody contiene nome della crag, contenuto in un tr
+        const cragsRaw = tab.querySelectorAll("tbody tr a") // lista dei tr contenenti il nome della falesia in a
+        for (let index = 0; index < cragsRaw.length; index++) { // per ogni crag
+          const crag = cragsRaw[index];
+          const link = crag.href
+          console.log('try')
+          const response = await axios.put('climbook',{link})
+          console.log('passed')
+        }
+        console.log('cragsraw:')
+        cragsRaw.forEach(c=>console.log('Crag Name:',c.textContent,', Regione:',region, ', Provincia:',provincia,', Link:',c.href))
+        // posso avere per ogni crag la sua regione e provincia
+        console.log('-------')
+      }
+
+      
+      
+      const provinceRaw = doc.querySelectorAll("th.px-0")
+      provinceRaw.forEach(r=>{
+        if(r.className ==='px-0 py-0 md:py-1 text-xl font-bold'){
+          province.push(r.textContent)}
+        })
+      
+    }
+
+    const doc = parser.parseFromString(result.data, 'text/html');
+    
     const regions = doc.querySelectorAll("title")
+    
 
     for (let index = 0; index < regions.length; index++) {
+
       const elem = regions[index]
       const string = elem.textContent
       const length = string.length
-        console.log(string.slice(8,length-11))
+       
     }
   }
 
